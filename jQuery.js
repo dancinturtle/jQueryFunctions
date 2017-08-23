538d1da2b59d2755c6b536288170c64c4461309e
McCabe <mccabe@wambl.com>
2017-08-23 15:26:57 -0700

    updated with more functions at bottom

--- jQuery.js
+++ jQuery.js
@@ -19,3 +19,55 @@ $('.slideToggleButton').click(function() {
 $('.appendButton').click(function() {
   $('.para3').clone().appendTo(".para3");
 });
+
+$('.html').click(function() {
+  $(".html").hide();
+  $('.show').show();
+});
+$('.show').click(function() {
+  $(".html").show();
+  $('.show').hide();
+});
+
+$("#get_attr").click(function() {
+  var val = $('.red').attr('class')
+  console.log(val);
+  $('.red').append('.red')
+
+});
+
+
+$('#set_button').click(function() {
+  var value = $('.red').attr('class')
+  if (value == 'red') {
+    $('.red').attr('class', 'yellow');
+  } else {
+    $('.red').attr('class', 'red');
+  }
+
+  //  $('.red').attr("class", "yellow");
+});
+
+$('#data').data('power', '0') // sets power to 0
+$('#get_power').click(function() {
+  var power = $('#data').data('power') * 0;
+  $('#power').text(power);
+  console.log(power);
+})
+
+$('#add_power').click(function() {
+  var power = $('#data').data('power');
+  $('#data').data('power', parseInt(power) + 10);
+  var power = $('#data').data('power');
+  console.log(power);
+
+  $('#power').text(power);
+
+})
+$('#remove_power').click(function() {
+  var power = $('#data').data('power');
+  $('#data').data('power', parseInt(power) - 10);
+  var power = $('#data').data('power');
+  console.log(power);
+  $('#power').text(power);
+})
